891fe9f1d2c6f5a466bf3680b386acbb6b831993
Nikolay Stanev <nikolay.stanev.255@gmail.com>
2023-04-01 16:38:13 +0300

    Added apache common utils to maven

--- src/main/java/com/reststar/RestStarApplication.java
+++ src/main/java/com/reststar/RestStarApplication.java
@@ -5,9 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RestStarApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(RestStarApplication.class, args);
-	}
-
+		SpringApplication.run(RestStarApplication.class, args);	}
 }
