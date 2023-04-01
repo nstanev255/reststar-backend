593dff6ba5fdbbd46ba76ac1651d75a8ab72a8f1
Nikolay Stanev <nikolay.stanev.255@gmail.com>
2023-04-01 16:24:33 +0300

    Initial Commit

---
+++ src/main/java/com/reststar/RestStarApplication.java
@@ -0,0 +1,13 @@
+package com.reststar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestStarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestStarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reststar/RestStarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reststar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestStarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
