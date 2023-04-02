b5524070fb7d0191bae8adf7122888afd65d8bc6
Nikolay Stanev <nikolay.stanev.255@gmail.com>
2023-04-02 17:28:09 +0300

    Added update for user information.

--- src/main/java/com/reststar/dto/UserInformationDTO.java
+++ src/main/java/com/reststar/dto/UserInformationDTO.java
@@ -1,11 +1,14 @@
 package com.reststar.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
 @Data
 public class UserInformationDTO {
     private String email;
     private Long userId;
+    @JsonIgnore
+    private String username;
     private String imageId;
     private String bannerId;
 }
--- src/main/java/com/reststar/repository/UserInformationRepository.java
+++ src/main/java/com/reststar/repository/UserInformationRepository.java
@@ -4,4 +4,5 @@ import com.reststar.entity.UserInformation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserInformationRepository extends JpaRepository<UserInformation, Long> {
+    UserInformation findByUserEntity_Id(Long id);
 }
